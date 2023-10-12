21a134c204c0433a7766318e12e39d65d64070b2
Denislav Tsenov <cenov.denislav@gmail.com>
2023-10-12 21:55:46 +0300

    Pathfinder
    some updates

--- pathfinder/src/main/java/com/web/pathfinder/web/UserRegisterController.java
+++ pathfinder/src/main/java/com/web/pathfinder/web/UserRegisterController.java
@@ -2,7 +2,9 @@ package com.web.pathfinder.web;
 
 import com.web.pathfinder.model.dto.UserRegisterDTO;
 import com.web.pathfinder.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +25,8 @@ public class UserRegisterController {
     }
 
     @PostMapping("/register")
-    public String register(UserRegisterDTO userRegisterDTO) {
+    public String register(@Valid UserRegisterDTO userRegisterDTO,
+                           BindingResult bindingResult) {
 
         userService.registerUser(userRegisterDTO);
 
